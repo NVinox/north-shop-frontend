a3c7b4d94f322121cbd9f62f1f8f08421a84cce8
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2026-03-13 03:20:57 +0700

    feat: добавил два блока с продуктами

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,19 @@ export default function Home() {
 		<>
 			<MainSlider />
 			<ProductList
-				className="pt-[40px] pb-[80px] lg:pt-[80px] lg:pb-[120px] md:pt-[60px] md:pb-[100px]"
+				className="pt-[40px] lg:pt-[80px] md:pt-[60px]"
 				title="Акции"
 				titleLink="Все акции"
+			/>
+			<ProductList
+				className="pt-[80px] lg:pt-[120px] md:pt-[100px]"
+				title="Новинки"
+				titleLink="Все новинки"
+			/>
+			<ProductList
+				className="pt-[80px] lg:pt-[120px] md:pt-[100px]"
+				title="Покупали раньше"
+				titleLink="Все покупки"
 			/>
 		</>
 	);
