ff16478c8147006e28a8cdef3274c8729e9c6990
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2026-03-18 02:09:56 +0700

    feat: доавил карту

--- src/widgets/shopsSection/ui/ShopsSection.tsx
+++ src/widgets/shopsSection/ui/ShopsSection.tsx
@@ -1,3 +1,5 @@
+import { Map } from "@/entities/map";
+
 import { Tabs, ADDRESSES } from "@/features/tabs";
 
 import { LayoutContainer } from "@/shared/layoutContainer";
@@ -12,11 +14,15 @@ export default function ShopsSection({ className = "" }: IProps) {
 			<LayoutContainer>
 				<h2 className="header-second margin-second-header">Наши магазины</h2>
 
-				<Tabs
-					inputName="addresses"
-					checkedValue="shchelyaur"
-					tabs={ADDRESSES}
-				/>
+				<div className="flex flex-col gap-y-[24px]">
+					<Tabs
+						inputName="addresses"
+						checkedValue="shchelyaur"
+						tabs={ADDRESSES}
+					/>
+
+					<Map />
+				</div>
 			</LayoutContainer>
 		</section>
 	);
